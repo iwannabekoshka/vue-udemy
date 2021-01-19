c87ba76b3695259613709331269848e2905eaff3
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-01-19 15:00:09 +0300

    2. Работа с ивентами (on:click)

---
+++ 2-basics/2-project-2/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			counter: 0,
+		};
+	},
+	methods: {
+		increase(num) {
+			this.counter = this.counter + num
+		},
+		reduce(num) {
+			this.counter = this.counter - num
+		}
+	}
+});
+
+app.mount('#events');
