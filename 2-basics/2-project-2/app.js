98c601b735e5eab3186541bf6962d0403f4735c6
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-01-25 11:47:45 +0300

    2. В колбэки можно пихать как евент, так и аргумент ($event, ARGUMENTI)

--- 2-basics/2-project-2/app.js
+++ 2-basics/2-project-2/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
 	data() {
 		return {
 			counter: 0,
+			firstName: 'dadadad',
 		};
 	},
 	methods: {
+		setName(event, lastName) {
+			this.firstName = event.target.value + ' ' + lastName
+		},
 		increase(num) {
 			this.counter = this.counter + num
 		},
