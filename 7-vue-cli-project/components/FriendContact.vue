772f6d089af327e17a8fa88a80fdc109a1b2cfc6
Daniil Pokusaeff <pokusaeff.daniil@ya.ru>
2021-02-09 14:51:20 +0300

    7. init + уии компоненты

---
+++ 7-vue-cli-project/components/FriendContact.vue
@@ -0,0 +1,57 @@
+<template>
+	<li>
+		<h2>{{friendName}} {{friendFavourite ? '(Fav)' : ''}}</h2>
+		<button @click="toggleFavourite">{{friendFavourite ? 'Unfav' : 'Fav'}}</button>
+		<button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+		<ul v-if="detailsAreVisible">
+			<li><strong>Phone:</strong> {{friendPhone}}</li>
+			<li><strong>E-mail:</strong> {{friendEmail}}</li>
+		</ul>
+	</li>
+</template>
+
+<script>
+	export default {
+		name: 'friend-contact',
+		props: {
+			friendId: {
+				type: Number,
+				required: true
+			},
+			friendName: {
+				type: String,
+				required: true
+			},
+			friendPhone: {
+				type: String,
+				required: true
+			},
+			friendEmail: {
+				type: String,
+				required: true
+			},
+			friendFavourite: {
+				type: Boolean,
+				required: false,
+				default: true
+			}
+		},
+		data() {
+			return {
+				detailsAreVisible: false,
+			}
+		},
+		methods: {
+			toggleDetails() {
+				this.detailsAreVisible = !this.detailsAreVisible
+			},
+			toggleFavourite() {
+				this.$emit('toggle-favourite', this.friendId)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ 7-vue-cli-project/src/App.vue
@@ -0,0 +1,127 @@
+<template>
+	<header>
+		<h1>My colleagues</h1>
+	</header>
+	<ul>
+		<friend-contact
+			v-for="friend in friends"
+			:key = friend.id
+			:friend-id="friend.id"
+			:friend-name="friend.name"
+			:friend-phone="friend.phone"
+			:friend-email="friend.email"
+			:friend-favourite="friend.favourite"
+			@toggle-favourite="toggleFavourite"
+		/>
+	</ul>
+</template>
+
+<script>
+	import FriendContact from "../components/FriendContact";
+	export default {
+		components: {FriendContact},
+		data () {
+			return {
+				friends: [
+					{
+						id: 1,
+						name: 'Sasha Anchous',
+						phone: '359-1337-1900',
+						email: 'nagibatorzemli3000@kek.com',
+						favourite: 1,
+					},
+					{
+						id: 2,
+						name: 'Nastya Unicorn',
+						phone: '100-149-247',
+						email: 'cornflowerblue@purrple.com',
+						favourite: true,
+					},
+					{
+						id: 3,
+						name: 'Iluha Master',
+						phone: 'nonofurbusiness',
+						email: 'yatvoyubdshatal@lk.com',
+						// favourite:
+					}
+				]
+			}
+		},
+		methods: {
+			toggleFavourite(friendId) {
+				const identifiedFriend = this.friends.find((friend) => friend.id === friendId);
+				identifiedFriend.favourite = !identifiedFriend.favourite
+			}
+		}
+	}
+</script>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+
+	html {
+		font-family: monospace;
+		font-size: 16px;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	header {
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+		margin: 3rem auto;
+		border-radius: 10px;
+		padding: 1rem;
+		background-color: cornflowerblue;
+		color: white;
+		text-align: center;
+		width: 90%;
+		max-width: 40rem;
+	}
+
+	#app ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+	#app li {
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+		margin: 1rem auto;
+		border-radius: 10px;
+		padding: 1rem;
+		text-align: center;
+		width: 90%;
+		max-width: 40rem;
+	}
+
+	#app h2 {
+		font-size: 2rem;
+		border-bottom: 4px solid #ccc;
+		color: #58004d;
+		margin: 0 0 1rem 0;
+	}
+
+	#app button {
+		font: inherit;
+		cursor: pointer;
+		border: 1px solid cornflowerblue;
+		background-color: cornflowerblue;
+		color: white;
+		padding: 0.2rem 1rem;
+		box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+	}
+
+	#app button:hover,
+	#app button:active {
+		background-color: #729dec;
+		border-color: #729dec;
+		box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+	}
+	#app button:focus {
+		outline: none;
+	}
+</style>
